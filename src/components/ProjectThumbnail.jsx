f1f97e42a12c14f7cdf74b8b847f6326960ddd3a
Stanislav <Home@Stanislavs-MacBook-Pro.local>
2018-09-11 21:20:58 +0300

    my first check with aleksandra. Carousel is still in progress / doesn't support dynamic content

--- src/components/ProjectThumbnail.jsx
+++ src/components/ProjectThumbnail.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import { Image, Col, Panel, Modal } from 'react-bootstrap'
 import './ProjectThumbnail.css'
+import ProductModal from './ProductModal.jsx';
 
 
 export default class ProjectThumbnail extends Component {
     constructor(props){
         super(props)
     }
+
     render(){
+        console.log(this.props.productPhotos)
         return(
             <Col xs={6} md={4} className="project-info" id="project-info">
-                <Image responsive src={this.props.src} alt="TODO" />
+                <ProductModal src={this.props.src} productPhotos={this.props.productPhotos} />
                 <h3>Project 1</h3>
                 <p>является текст-заполнитель обычно используется в графических, печать и издательской</p>
             </Col>
