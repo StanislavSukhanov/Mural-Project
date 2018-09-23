1696f75623b5444edc9b6598557a907b9742ba80
Stanislav <Home@Stanislavs-MacBook-Pro.local>
2018-09-23 12:18:28 +0300

    inserted a slider from one tutorial https://medium.com/@ItsMeDannyZ/build-an-image-slider-with-react-es6-264368de68e4 still in process of making it work

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ import ProjectThumbnail from './ProjectThumbnail.jsx'
 import './Home.css'
 
 // for DEV mocde 
-const productPhotos = []
-productPhotos.push("https://i.ytimg.com/vi/NVhQhIVtvwU/hqdefault.jpg")
-productPhotos.push("https://img00.deviantart.net/10e8/i/2007/337/8/f/drawing_on_the_wall_by_lvjonok.jpg")
-productPhotos.push("https://previews.123rf.com/images/nexusplexus/nexusplexus1308/nexusplexus130800506/21247140-back-view-of-businessman-drawing-sketch-on-wall.jpg")
-productPhotos.push("https://ae01.alicdn.com/kf/HTB1l0DSOVXXXXXBXFXXq6xXFXXXY/Funny-Smile-Face-wall-Decal-Mural-Art-Home-Decor-Bathroom-Sticker-Halloween-decoration-Gift-8495.jpg")
+const images = []
+images.push("https://i.ytimg.com/vi/NVhQhIVtvwU/hqdefault.jpg")
+images.push("https://img00.deviantart.net/10e8/i/2007/337/8/f/drawing_on_the_wall_by_lvjonok.jpg")
+images.push("https://previews.123rf.com/images/nexusplexus/nexusplexus1308/nexusplexus130800506/21247140-back-view-of-businessman-drawing-sketch-on-wall.jpg")
+images.push("https://ae01.alicdn.com/kf/HTB1l0DSOVXXXXXBXFXXq6xXFXXXY/Funny-Smile-Face-wall-Decal-Mural-Art-Home-Decor-Bathroom-Sticker-Halloween-decoration-Gift-8495.jpg")
 
-console.log(productPhotos[0])
-productPhotos.map(el =>{
+console.log(images[0])
+images.map(el =>{
     return console.log(el)
 })
 
@@ -29,12 +29,12 @@ export default class Home extends Component {
                 </Jumbotron>
             <Row>
                 {/* This will be dynamic as soon as we have a Database */}
-                <ProjectThumbnail productPhotos={productPhotos} src="http://www.artnak.net/th/drawing-wall-stickers-living-room-bedroom-wall-decor-bedroom-wall-paintings-drawings_1522022816_370x248_5c30576e3e3e6562.jpg"/>
-                <ProjectThumbnail productPhotos={productPhotos} src="https://1.bp.blogspot.com/-ZN3FVnnN9dE/TxLbi0Ktt8I/AAAAAAAAAWU/6IE4gSAhPiE/s1600/6e774__Large-Tree-Wall-Decals-Beds.jpg"/>
-                <ProjectThumbnail productPhotos={productPhotos} src="http://zdrasti.club/wp-content/uploads/2017/09/bedroom-wall-drawings-bedroom-ideas-with-black-wall-decals-wall-mural-decals-are-the-alternative-way-to-make-cool-bedroom-wall-drawings.jpg"/>
-                <ProjectThumbnail productPhotos={productPhotos} src="https://www.guiaparadecorar.com/wp-content/uploads/2018/06/Modelos-de-papel-mural-para-la-habitacion-matrimonial10.jpg"/>
-                <ProjectThumbnail productPhotos={productPhotos} src="http://zdrasti.club/wp-content/uploads/2017/09/bedroom-wall-drawings-wall-painting-tree-with-birds-wall-painting-bedroom-wall-drawings-tumblr.jpg"/>
-                <ProjectThumbnail productPhotos={productPhotos} src="images/project1.jpg"/>
+                <ProjectThumbnail images={images} src="http://www.artnak.net/th/drawing-wall-stickers-living-room-bedroom-wall-decor-bedroom-wall-paintings-drawings_1522022816_370x248_5c30576e3e3e6562.jpg"/>
+                <ProjectThumbnail images={images} src="https://1.bp.blogspot.com/-ZN3FVnnN9dE/TxLbi0Ktt8I/AAAAAAAAAWU/6IE4gSAhPiE/s1600/6e774__Large-Tree-Wall-Decals-Beds.jpg"/>
+                <ProjectThumbnail images={images} src="http://zdrasti.club/wp-content/uploads/2017/09/bedroom-wall-drawings-bedroom-ideas-with-black-wall-decals-wall-mural-decals-are-the-alternative-way-to-make-cool-bedroom-wall-drawings.jpg"/>
+                <ProjectThumbnail images={images} src="https://www.guiaparadecorar.com/wp-content/uploads/2018/06/Modelos-de-papel-mural-para-la-habitacion-matrimonial10.jpg"/>
+                <ProjectThumbnail images={images} src="http://zdrasti.club/wp-content/uploads/2017/09/bedroom-wall-drawings-wall-painting-tree-with-birds-wall-painting-bedroom-wall-drawings-tumblr.jpg"/>
+                <ProjectThumbnail images={images} src="images/project1.jpg"/>
             </Row>
 
             </Grid>
--- src/components/ProjectThumbnail.jsx
+++ src/components/ProjectThumbnail.jsx
@@ -4,6 +4,7 @@ import './ProjectThumbnail.css'
 import ProductModal from './ProductModal.jsx';
 
 
+
 export default class ProjectThumbnail extends Component {
     constructor(props){
         super(props)
@@ -11,18 +12,18 @@ export default class ProjectThumbnail extends Component {
 
     render(){
 
-        console.log(this.props.productPhotos)
-        if (this.props.productPhotos) console.log("great")
-        this.props.productPhotos.map(el => {
+        console.log(this.props.images)
+        if (this.props.images) console.log("great")
+        this.props.images.map(el => {
             return console.log(el)
         })
-        //const productPhotos = this.props.productPhotos.concat()
-        //console.log(productPhotos instanceof Array ) // now true
-        //const productPhotos = this.props.productPhotos
+        //const images = this.props.images.concat()
+        //console.log(images instanceof Array ) // now true
+        //const images = this.props.images
         let hello = "hello"
         return(
             <Col xs={6} md={4} className="project-info" id="project-info">
-                <ProductModal src={this.props.src} productPhotos={this.props.productPhotos} hello={hello}/>
+                <ProductModal src={this.props.src} images={this.props.images} hello={hello}/>
            </Col>
         )
     }
